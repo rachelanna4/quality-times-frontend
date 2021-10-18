7d9a669f780d965d6d2ac1037b2051cc55428574
Rachel <rachelanna92@outlook.com>
2021-10-18 15:30:33 +0100

    add basic component structure and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import { Switch, Route } from "react-router-doms";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import TopicsGallery from "./components/TopicsGallery";
+import Articles from "./components/Articles";
+import SingleArticle from "./components/Articles";
+import PostArticle from "./components/PostArticle";
+import PostComment from "./components/PostComment";
+import EditComment from "./components/EditComment";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <TopicsGallery />
+        </Route>
+        <Route exact path="/articles">
+          <Articles />
+        </Route>
+        <Route exact path="/articles/:topic_slug">
+          <Articles />
+        </Route>
+        <Route exact path="/articles/:article_id">
+          <SingleArticle />
+        </Route>
+        <Route exact path="/articles/:article_id/post-comment">
+          <PostComment />
+        </Route>
+        <Route exact path="/articles/:article_id/edit-comment">
+          <EditComment />
+        </Route>
+        <Route exact path="/post-article">
+          <PostArticle />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
