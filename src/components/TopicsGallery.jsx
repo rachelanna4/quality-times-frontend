548bc5b7770c2756c73c2c2ce56f6db8836220b1
Rachel <rachelanna92@outlook.com>
2021-10-18 18:04:37 +0100

    display default image for topic

--- src/components/TopicsGallery.jsx
+++ src/components/TopicsGallery.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { getTopics }  from "../utils/api.js"
 
 const TopicsGallery = () => {
-const [topics, setTopics] = useState([])
+const [topics, setTopics] = useState([]);
+const [isError, setIsError] = useState(false);
+const history = useHistory();
 
 useEffect( () => {
-    getTopics().then((topicsFromApi)=> {
+    setIsError(false);
+    getTopics()
+    .then((topicsFromApi)=> {
         setTopics(topicsFromApi)
     })
+    .catch(() => {
+        setIsError(true);
+      });
 }, [])
 
 
     return (
         <section className="TopicsGallery">
-            <h2>Latest News:</h2>
+          {isError ? (
+            <p>Oops! Something went wrong.</p>
+          ) : (
             <ul>
-            {topics.map((topic) => {
+              {topics.map((topic) => {
                 return (
-                    <li key={topic.slug}>
-                        <img src={`/images/${topic.slug}.jpg`} alt={topic.slug}></img>
+                    <li key={topic.slug} >
+                        <img 
+                          src={`/images/${topic.slug}.jpg`}
+                          onError={((e) => {
+                            e.target.src="/images/default.jpg"
+                            })}
+                          alt={topic.slug}></img>
                         <h3>{topic.slug}</h3>
                     </li> 
                 )
-            })}
+              })}
             </ul>
+        )}
         </section>
     );
 };
