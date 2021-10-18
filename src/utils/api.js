64f585cb9b1e44a1bb0da5290243cbc8766bf66f
Rachel <rachelanna92@outlook.com>
2021-10-18 19:11:58 +0100

    enable getArticles function to accept multiple query paramaters when making a request to the API

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,21 @@ const newsApi = axios.create({
   baseURL: "https://the-quality-times.herokuapp.com/api",
 });
 
+export const getArticles = async (...params) => {
+  const queries = {};
+
+  params.forEach((param) => {
+    Object.entries(param).forEach(([key, value]) => {
+      queries[key] = value;
+    });
+  });
+
+  console.log(queries);
+
+  const { data } = await newsApi.get("/articles", { params: queries });
+  return data.articles;
+};
+
 export const getTopics = async () => {
   const { data } = await newsApi.get("/topics");
   return data.topics;
