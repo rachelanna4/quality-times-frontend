9b8382d9a77472b707c0043658a2df650bb19dcb
Rachel <rachelanna92@outlook.com>
2021-10-18 16:34:59 +0100

    add h1 and p text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="header">
+            <h1>The Quality Times</h1>
+            <p>Bringing you the latest uplifting and inspiring news from around the globe</p>
+        </section>
     );
 };
 
