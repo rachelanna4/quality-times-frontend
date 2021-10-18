be90b08a3ed16eb1fd606fe570cdc578a0e530c0
Rachel <rachelanna92@outlook.com>
2021-10-18 16:35:12 +0100

    add nav bar links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/articles">Articles </Link>
+            <Link to="/post-article">Post Article</Link>
+        </section>
     );
 };
 
