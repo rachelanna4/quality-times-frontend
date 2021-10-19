460eb83b4dd3540e3d34852df29d9e85c79e9e9c
Rachel <rachelanna92@outlook.com>
2021-10-19 15:45:29 +0100

    add error handling to display message on card when articles cannot be loaded

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { getArticles } from '../utils/api'; 
 
 const Articles = () => {
@@ -19,20 +21,36 @@ useEffect(() => {
       });
 }, [topic] )
 
+
     return (
-        <section className="ArticlesList">
-        <ul>
-            {allArticles.map((article) => {
-                return (
-                    <li key={article.article_id}>
-                        <h3>{article.title}</h3>
-                        <p>{article.author}</p>
-                        <p>{article.topic}</p>
-                    </li>
-                )
-            })}
-        </ul>
-        </section>
+            <section className="ArticlesList">
+                {isError ? (
+                    <Card className="Card">
+                    <Card.Img className="Card_img" variant="top" src="/images/latest-news.png" />
+                    <Card.Title className="Card_title">Sorry we can't find any articles right now</Card.Title>
+                    <Card.Body>
+                      <Card.Text>
+                        Please try again later 
+                      </Card.Text>
+                      <Button className="Card_button" variant="primary">Go back home</Button>
+                    </Card.Body>
+                  </Card>
+                ) : (
+            <section className="ArticlesList">
+                    <ul>
+                        {allArticles.map((article) => {
+                            return (
+                                <li key={article.article_id}>
+                                    <h3>{article.title}</h3>
+                                    <p>{article.author}</p>
+                                    <p>{article.topic}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    </section>
+                )}
+            </section> 
     );
 };
 
