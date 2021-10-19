9b0afe39c2d2d6a4b53a8b0e059ce1e0016611b4
Rachel <rachelanna92@outlook.com>
2021-10-19 12:55:33 +0100

    add home icon to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { HouseDoor } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+
 
 const Nav = () => {
     return (
-        <section className="nav">
-            <Link to="/">Home</Link>
+        <section className="Nav">
+            <Link to="/"><HouseDoor /> Home</Link>
             <Link to="/articles">Articles </Link>
             <Link to="/post-article">Post Article</Link>
         </section>
