38698074d5a48560c316ac26f03305f8c3c86610
Rachel <rachelanna92@outlook.com>
2021-10-19 14:59:06 +0100

    implement functionality to return either all articles or articles only from specified topic when selected

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route exact path="/articles">
           <Articles />
         </Route>
-        <Route exact path="/articles/topics/:topic_slug">
+        <Route exact path="/articles/topics/:topic">
           <Articles />
         </Route>
         <Route exact path="/articles/:article_id">
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getArticles } from '../utils/api'; 
 
 const Articles = () => {
+const [allArticles, setAllArticles] = useState([])
+const [isError, setIsError] = useState(false);
+const { topic } = useParams();
+
+useEffect(() => {
+    setIsError(false);
+    getArticles({topic: topic})
+    .then((articlesFromApi) => {
+        setAllArticles(articlesFromApi)
+    })
+    .catch(() => {
+        setIsError(true);
+      });
+}, [topic] )
+
     return (
-        <div>
-            
-        </div>
+        <section className="ArticlesList">
+        <ul>
+            {allArticles.map((article) => {
+                return (
+                    <li key={article.article_id}>
+                        <h3>{article.title}</h3>
+                        <p>{article.author}</p>
+                        <p>{article.topic}</p>
+                    </li>
+                )
+            })}
+        </ul>
+        </section>
     );
 };
 
