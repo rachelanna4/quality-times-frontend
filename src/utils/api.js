621d6077070330b872400168df644c13437e8465
Rachel <rachelanna92@outlook.com>
2021-10-20 13:26:20 +0100

    remove commented out code

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,14 +11,6 @@ export const getArticles = async (queries) => {
 
   const newQueries = { ...queries, page, limit, offset };
 
-  // const queries = params[0] {limit, offset};
-
-  // params.forEach((param) => {
-  //   Object.entries(param).forEach(([key, value]) => {
-  //     queries[key] = value;
-  //   });
-  // });
-
   const { data } = await newsApi.get("/articles", { params: newQueries });
 
   return data;
