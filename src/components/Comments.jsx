0cde004346447cb994b7ff2ed58c799bd74148bd
Rachel <rachelanna92@outlook.com>
2021-10-21 12:28:26 +0100

    move comments functionality into its own component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import * as api from '../utils/api'; 
+import { PersonFill, Calendar, ChatLeftText, } from 'react-bootstrap-icons';
+import Accordion from 'react-bootstrap/Accordion';
+
+
+const Comments = ({article_id, comment_count}) => {
+    const [comments, setComments] = useState([])
+    const [isCommentsError, setIsCommentsError] = useState(false);
+
+    useEffect(() => {
+        setIsCommentsError(false);
+        api.getCommentsByArticle(article_id)
+        .then((commentsFromApi) => {
+            setComments(commentsFromApi.comments)
+        })
+        .catch(() => {
+            setIsCommentsError(true);
+        });
+    }, [comments, article_id])
+
+
+    return (
+        <section className="SingleArticle_columns-rightBottom">
+                        {isCommentsError ? (
+                            <section>
+                                <Accordion className="CommentList-error">
+                                    <Accordion.Item eventKey="comments-error">
+                                        <Accordion.Header className="CommentList_header-error">
+                                            <p>
+                                                <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all comments
+                                            </p>
+                                        </Accordion.Header>
+                                        <Accordion.Body>
+                                                <p>Sorry we can't find any comments right now</p>
+                                                <p>Try again later</p>
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </section>
+                        ) : (
+                            <section>
+                                <Accordion className="CommentList">
+                                <Accordion.Item eventKey="comments">
+                                    <Accordion.Header className="CommentList_header">
+                                    <p>
+                                        <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all {comment_count} comments
+                                    </p>
+                                    </Accordion.Header>
+                                    {comments.map((comment) => {
+                                        return (
+                                            <Accordion.Body >
+                                                <section className="CommentList_metadata">
+                                                    <p>
+                                                        <span role="img" aria-label="Date" className="CommentDate_icon"><Calendar/></span> &nbsp;&nbsp;{new Date(comment.created_at).toLocaleDateString("en-GB")}
+                                                    </p>
+                                                    <p>
+                                                        <span role="img" aria-label="Written by"><PersonFill className="CommentAuthor_icon"/></span>&nbsp;&nbsp;{comment.author} 
+                                                    </p>
+                                                </section>
+                                                    <section className="CommentList_body">
+                                                    <p>"{comment.body}"</p>
+                                                </section>
+                                                <p>Votes: {comment.votes}</p>
+                                                <hr />
+                                            </Accordion.Body>
+                                        )
+                                    })}
+                                
+                                </Accordion.Item>
+                                </Accordion>
+                            </section>
+                        )}
+                    </section>
+    );
+};
+
+export default Comments;
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 import * as api from '../utils/api'; 
-import { PersonFill, Calendar, ChatLeftText, StarFill } from 'react-bootstrap-icons';
-import Accordion from 'react-bootstrap/Accordion';
+import Comments from "./Comments";
+import { PersonFill, Calendar, StarFill } from 'react-bootstrap-icons';
 
 const SingleArticle = () => {
     const {article_id} = useParams()
     const [article, setArticle] = useState("")
-    const [comments, setComments] = useState([])
     const [isArticleError, setIsArticleError] = useState(false);
-    const [isCommentsError, setIsCommentsError] = useState(false);
 
     useEffect(() => {
         setIsArticleError(false);
@@ -21,19 +19,6 @@ const SingleArticle = () => {
             setIsArticleError(true);
         });
     }, [article_id])
-
-    useEffect(() => {
-        setIsCommentsError(false);
-        api.getCommentsByArticle(article_id)
-        .then((commentsFromApi) => {
-            setComments(commentsFromApi.comments)
-        })
-        .catch(() => {
-            setIsCommentsError(true);
-        });
-    }, [comments, article_id])
-
-
 
     return (
         <> 
@@ -63,58 +48,7 @@ const SingleArticle = () => {
                         <span role="img" aria-label="Number of stars"><StarFill className="ArticleStars_icon" /></span>&nbsp;&nbsp;{article.votes}
                     </p>
                     </section>
-                    <section className="SingleArticle_columns-rightBottom">
-                        {isCommentsError ? (
-                            <section>
-                                <Accordion className="CommentList-error">
-                                    <Accordion.Item eventKey="comments-error">
-                                        <Accordion.Header className="CommentList_header-error">
-                                            <p>
-                                                <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all comments
-                                            </p>
-                                        </Accordion.Header>
-                                        <Accordion.Body>
-                                                <p>Sorry we can't find any comments right now</p>
-                                                <p>Try again later</p>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </section>
-                        ) : (
-                            <section>
-                                <Accordion className="CommentList">
-                                <Accordion.Item eventKey="comments">
-                                    <Accordion.Header className="CommentList_header">
-                                    <p>
-                                        <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all {article.comment_count} comments
-                                    </p>
-                                    </Accordion.Header>
-                                    {comments.map((comment) => {
-                                        return (
-                                            <Accordion.Body >
-                                                <section className="CommentList_metadata">
-                                                    <p>
-                                                        <span role="img" aria-label="Date" className="CommentDate_icon"><Calendar/></span> &nbsp;&nbsp;{new Date(comment.created_at).toLocaleDateString("en-GB")}
-                                                    </p>
-                                                    <p>
-                                                        <span role="img" aria-label="Written by"><PersonFill className="CommentAuthor_icon"/></span>&nbsp;&nbsp;{comment.author} 
-                                                    </p>
-                                                </section>
-                                                    <section className="CommentList_body">
-                                                    <p>"{comment.body}"</p>
-                                                </section>
-                                                <p>Votes: {comment.votes}</p>
-                                                <hr />
-                                            </Accordion.Body>
-                                        )
-                                    })}
-                                
-                                </Accordion.Item>
-                                </Accordion>
-                            </section>
-                        )}
-                    </section>
-                        
+                    <Comments article_id={article_id} comment_count={article.comment_count} />    
                 </section>
             </section>
             )
