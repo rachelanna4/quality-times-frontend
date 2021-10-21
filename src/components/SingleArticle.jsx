2de99c6ae4d59827c12d9454202dab07ea16d0f3
Rachel <rachelanna92@outlook.com>
2021-10-21 11:02:33 +0100

    add error handling for rendering comments

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -64,28 +64,46 @@ const SingleArticle = () => {
                     </p>
                     </section>
                     <section className="SingleArticle_columns-rightBottom">
-                            <Accordion>
-                            <Accordion.Item eventKey="comments">
-                                <Accordion.Header>
-                                <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all {article.comment_count} comments
-                                </Accordion.Header>
-                                {comments.map((comment) => {
-                                    return (
+                        {isCommentsError ? (
+                            <section className="CommentList-error">
+                                <Accordion>
+                                    <Accordion.Item eventKey="comments-error">
+                                        <Accordion.Header>
+                                            <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all comments
+                                        </Accordion.Header>
                                         <Accordion.Body>
-                                        <p>
-                                            <span role="img" aria-label="Written by"><PersonFill className="CommentAuthor_icon"/></span>{comment.author} 
-                                            <span role="img" aria-label="Date" className="CommentDate_icon"><Calendar/></span> {new Date(comment.created_at).toLocaleDateString("en-GB")}
-                                        </p>
-                                        <p>{comment.body}</p>
-                                        <p>Votes: {comment.votes}</p>
+                                                <p>Sorry we can't find any comments right now</p>
+                                                <p>Try again later</p>
                                         </Accordion.Body>
-                                    )
-                                })}
-                               
-                            </Accordion.Item>
-                            </Accordion>
-
+                                    </Accordion.Item>
+                                </Accordion>
+                            </section>
+                        ) : (
+                            <section className="CommentList">
+                                <Accordion>
+                                <Accordion.Item eventKey="comments">
+                                    <Accordion.Header>
+                                    <span role="img" aria-label="Number of comments"><ChatLeftText className="ArticleComments_icon" /></span> &nbsp; &nbsp;View all {article.comment_count} comments
+                                    </Accordion.Header>
+                                    {comments.map((comment) => {
+                                        return (
+                                            <Accordion.Body>
+                                            <p>
+                                                <span role="img" aria-label="Written by"><PersonFill className="CommentAuthor_icon"/></span>{comment.author} 
+                                                <span role="img" aria-label="Date" className="CommentDate_icon"><Calendar/></span> {new Date(comment.created_at).toLocaleDateString("en-GB")}
+                                            </p>
+                                            <p>{comment.body}</p>
+                                            <p>Votes: {comment.votes}</p>
+                                            </Accordion.Body>
+                                        )
+                                    })}
+                                
+                                </Accordion.Item>
+                                </Accordion>
+                            </section>
+                        )}
                     </section>
+                        
                 </section>
             </section>
             )
