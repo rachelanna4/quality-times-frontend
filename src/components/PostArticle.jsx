b0984261642070b449bbe0ce6f1c11d8a26810ed
Rachel <rachelanna92@outlook.com>
2021-10-22 12:10:53 +0100

    request that user logs in before posting article

--- src/components/PostArticle.jsx
+++ src/components/PostArticle.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext, RequiresLogin, RequiresGuest} from '../contexts/User';
+import * as api from '../utils/api'; 
 
 const PostArticle = () => {
+    const { user, setUser, isLoggedIn} = useContext(UserContext); 
+    
     return (
-        <div>
-            
-        </div>
+        <>
+        <RequiresGuest isLoggedIn={isLoggedIn}>
+            <section className="PostArticleLogin">
+                <h4>Please login to post your article</h4>
+                <button onClick={ () => {
+                    setUser("jessjelly")
+                }
+                }>Login</button>
+            </section>
+        </RequiresGuest>
+        <RequiresLogin isLoggedIn={isLoggedIn}>
+            <section className="PostArticleForm-container">
+        
+            </section>
+        </RequiresLogin>
+        </>
     );
 };
 
