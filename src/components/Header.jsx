0862177f8d46bca12df0cbe9a2f566684b8d8368
Rachel <rachelanna92@outlook.com>
2021-10-25 08:54:02 +0100

    make header a clickable link to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <section className="Header">
+            <Link to="/" className="Header_link">
             <h1>The Quality Times</h1>
             <p>Bringing you the latest positive and inspiring news from around the globe</p>
+            </Link>
         </section>
     );
 };
